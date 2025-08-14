81c77ed08345cbb892f2abf1c81c6ddd0108f058
subhamayviaconteam <subhamay.aditya@viaconteam.com>
2025-08-14 14:46:26 +0530

    gtag script added on header

--- wp-content/themes/remote-work-theme/header.php
+++ wp-content/themes/remote-work-theme/header.php
@@ -10,6 +10,11 @@
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Fredoka:wght@300..700&display=swap" rel="stylesheet">
 
+
+  <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-3ENKD4L34K"></script>
+    <script>window.dataLayer = window.dataLayer || [];   function gtag(){dataLayer.push(arguments);}   gtag('js', new Date());   gtag('config', 'G-3ENKD4L34K'); </script>
+
   <?php wp_head(); ?>
 
 </head>
